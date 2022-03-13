f35d5956e6704e89a24dd695bbd17455e5040d62
Aleksei Aksenov <Aksenov-89@yandex.ru>
2022-03-13 15:29:28 +0500

    The project has changed according to the recomendations. Folder Migrations has deleted.

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -2,20 +2,20 @@ from django.shortcuts import get_object_or_404, render
 
 from .models import Group, Post
 
+LIMIT_POSTS = 10
+
 
 def index(request):
-    posts = Post.objects.order_by('-pub_date')[:10]
-    context = {
-        'posts': posts,
-    }
-    return render(request, 'posts/index.html', context)
+    posts = Post.objects.select_related('author').all()[:LIMIT_POSTS]
+    return render(request, 'posts/index.html',
+                  {'posts': posts, }
+                  )
 
 
 def group_posts(request, slug):
     group = get_object_or_404(Group, slug=slug)
-    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
-    context = {
-        'group': group,
-        'posts': posts,
-    }
-    return render(request, 'posts/group_list.html', context)
+    posts = group.post_group.all()[:LIMIT_POSTS]
+    return render(request, "posts/group_list.html",
+                  {"group": group,
+                   "posts": posts, }
+                  )
