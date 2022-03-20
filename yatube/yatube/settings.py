a8a1bb26aa0e3d4922418f29b4c7f44710de4864
Aleksei Aksenov <Aksenov-89@yandex.ru>
2022-03-20 21:06:30 +0500

    Some changes has made. Added templates Signup, Login, Logout, Password_change, Password_change/done

--- yatube/yatube/settings.py
+++ yatube/yatube/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'posts.apps.PostsConfig',
+    'users.apps.UsersConfig',
+    'core.apps.CoreConfig',
 ]
 
 MIDDLEWARE = [
@@ -105,7 +107,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'UTC'
 
@@ -122,3 +124,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'posts:index'
+LOGOUT_REDIRECT_URL = 'posts:index'  
--- yatube/yatube/urls.py
+++ yatube/yatube/urls.py
@@ -13,10 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path('', include('posts.urls', namespace='posts')),
     path('admin/', admin.site.urls),
+    # Все адреса с префиксом auth/ 
+    # будут перенаправлены в модуль django.contrib.auth
+    path('auth/', include('users.urls')),
+    path('auth/', include('django.contrib.auth.urls')),
+    path('', include('posts.urls', namespace='posts')),
 ]
